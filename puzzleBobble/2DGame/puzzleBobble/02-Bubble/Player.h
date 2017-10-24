5d04fe791aaa9465fe8677f84e6a1dc0b90bae0c
Xictrov <vimar_cam@hotmail.com>
2017-10-25 01:24:36 +0200

    clean commit

---
+++ puzzleBobble/2DGame/puzzleBobble/02-Bubble/Player.h
@@ -0,0 +1,36 @@
+#ifndef _PLAYER_INCLUDE
+#define _PLAYER_INCLUDE
+
+
+#include "Sprite.h"
+#include "TileMap.h"
+
+
+// Player is basically a Sprite that represents the player. As such it has
+// all properties it needs to track its movement, jumping, and collisions.
+
+
+class Player
+{
+
+public:
+	void init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram, int colorbola);
+	void update(int deltaTime, float angle, bool &cambio, bool &acaba);
+	void render();
+
+	void setTileMap(TileMap *tileMap);
+	void setPosition(const glm::vec2 &pos);
+
+private:
+	bool bJumping;
+	int color;
+	glm::ivec2 tileMapDispl, posPlayer;
+	int jumpAngle, startY;
+	Texture spritesheet;
+	Sprite *sprite;
+	TileMap *map;
+
+};
+
+
+#endif // _PLAYER_INCLUDE
---
+++ puzzleBobble/2DGame/puzzleBobble/02-Bubble/Scene.h
@@ -0,0 +1,49 @@
+#ifndef _SCENE_INCLUDE
+#define _SCENE_INCLUDE
+
+
+#include <glm/glm.hpp>
+#include "ShaderProgram.h"
+#include "TileMap.h"
+#include "Player.h"
+#include "Quad.h"
+#include "TexturedQuad.h"
+#include "Arrow.h"
+
+#define SCREEN_WIDTH 640.f
+#define SCREEN_HEIGHT 480.f
+
+
+// Scene contains all the entities of our game.
+// It is responsible for updating and render them.
+
+
+class Scene
+{
+
+public:
+	Scene();
+	~Scene();
+
+	void init();
+	void update(int deltaTime);
+	void render();
+
+private:
+	void initShaders();
+
+private:
+	TileMap *map;
+	Player *player;
+	Arrow *arrow;
+	ShaderProgram texProgram, simpleProgram;
+	float currentTime, angle;
+	glm::mat4 projection;
+	Quad *quad;
+	TexturedQuad *texQuad;
+	Texture texs;
+};
+
+
+#endif // _SCENE_INCLUDE
+
